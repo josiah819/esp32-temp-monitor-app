fa6ad262701cc0aa36c374d9c5dccef25ab09035
josiah819 <josiah819@gmail.com>
2025-08-12 20:46:19 -0400

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -5,12 +5,15 @@ export default {
     slug: "esp32-temp-monitor-app",
     version: "0.1.0",
     scheme: "esp32temp",
-    extra: { API_BASE_URL: "https://esp32-node-bridge-production.up.railway.app" }, // <— your Railway URL
+    extra: {
+      API_BASE_URL: "https://esp32-node-bridge-production.up.railway.app",
+      eas: { projectId: "<e5adda6d-fdac-4c0d-88fa-6bc8283512a2>" }
+    },
     android: {
-      package: "com.josiah.esp32tempmonitorapp", // all lowercase; must be unique
+      package: "com.josiah.esp32tempmonitorapp", // all lowercase, must be unique
       adaptiveIcon: { backgroundColor: "#ffffff" } // no foregroundImage
     },
-    // No 'icon' property -> Expo uses a default
-    splash: { backgroundColor: "#ffffff", resizeMode: "contain" } // no splash.image
+    // No icon/splash image paths so the build won't look for missing PNGs
+    splash: { backgroundColor: "#ffffff", resizeMode: "contain" }
   }
 };
